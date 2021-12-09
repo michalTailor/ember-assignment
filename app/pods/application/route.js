fb81e94b8f73286f6f2d1a199ad73dea9e608f05
Michal Aimelak <michala@tailorbrands.com>
2021-12-09 17:17:03 +0200

    connect to the ruby server

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -3,19 +3,22 @@ import fetch from 'fetch';
 
 export default class ApplicationRoute extends Route {
   async model() {
-    let response = await fetch(
-      'https://api.airtable.com/v0/appgnZDYMlgJNlGRI/Content?maxRecords=100&view=Content%20pipeline',
-      {
-        headers: {
-          Authorization: 'Bearer key1d9Cb2uhoepb1Q',
-        },
-      }
-    );
+    // let response = await fetch(
+    //   'https://api.airtable.com/v0/appgnZDYMlgJNlGRI/Content?maxRecords=100&view=Content%20pipeline',
+    //   {
+    //     headers: {
+    //       Authorization: 'Bearer key1d9Cb2uhoepb1Q',
+    //     },
+    //   }
+    // );
+
+    let response = await fetch('http://127.0.0.1:3000/');
+
     let data = await response.json();
     const res = [];
     let obj = {};
 
-    data.records.forEach((element) => {
+    data.forEach((element) => {
       obj = {
         headline: element.fields.Headline,
         subHeadline:
@@ -23,9 +26,9 @@ export default class ApplicationRoute extends Route {
             ? 'No sub-headline'
             : element.fields['Sub-headline'],
         imageUrl:
-          typeof element.fields['Header image'] === 'undefined'
+          typeof element.fields.HeaderImage === 'undefined'
             ? 'https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg'
-            : element.fields['Header image'][0].url,
+            : element.fields.HeaderImage,
       };
       res.push(obj);
 
